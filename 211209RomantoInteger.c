28c98a1f3a39437feadef178889ea546a1443d45
mark407417012 <95852699+mark407417012@users.noreply.github.com>
2021-12-09 20:54:53 +0800

    Update 211209RomantoInteger.c

--- 211209RomantoInteger.c
+++ 211209RomantoInteger.c
@@ -4,30 +4,79 @@
 int romanToInt(char*);
 
 int main(void) {
-    char r2i[80];
+    char r2i[128];
     scanf("%s",r2i);
-    printf("%d",romanToInt(r2i));
+    printf("s:%d",romanToInt(r2i));
 
 }
 int romanToInt(char * s){
     int count=0;
-    for(int i=0;i<sizeof(s);i++) {
+    for(int i=0;i<strlen(s);i++) {
         switch(s[i]) {
             case 'M':
-                count+=1000; break;
+                if(s[i-1]=='C') {
+                    count+=800;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                } else {
+                    count+=1000;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                }
             case 'D':
-                count+=500; break;
+                if(s[i-1]=='C') {
+                    count+=300;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                } else {
+                    count+=500;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                }
             case 'C':
-                count+=100; break;
+                if(s[i-1]=='X') {
+                    count+=80;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                } else {
+                    count+=100;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                }
             case 'L':
-                count+=50; break;
+                if(s[i-1]=='X') {
+                    count+=30;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                } else {
+                    count+=50;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                }
             case 'X':
-                count+=10; break;
+                if(s[i-1]=='I') {
+                    count+=8;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                } else {
+                    count+=10;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                }
             case 'V':
-                count+=5; break;
+                if(s[i-1]=='I') {
+                    count+=3;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                } else {
+                    count+=1;
+                    printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                    break;
+                }
             case 'I':
-                count+=1; break;
-
+                count+=1;
+                printf("count=%d, s[%d]=%c\n",count,i,s[i]);
+                break;
         }
     }
     return count;
